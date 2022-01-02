818f4dc6e394bd387d6452cc8351688e13a8a5ba
voomoo <rakibul15-1318@diu.edu.bd>
2022-01-02 22:04:00 +0600

    worked on user based data fetching

--- controllers/expense.js
+++ controllers/expense.js
@@ -1,87 +1,88 @@
-const Expense = require("../models/Expenses")
+const Expense = require("../models/Expenses");
 
 //@desc get all the expenses
 //@route GET api/v1/expense-tracker
 //@access PRIVATE
 exports.getExpenses = async (req, res, next) => {
-    try {
-        const expenses = await Expense.find();
-        res.status(200).json({
-            success:true,
-            data: expenses
-        })
-    } catch (error) {
-        res.status(400).json({
-            success:false
-        })
-    }
-}
+  try {
+    const expenses = await Expense.where("user").equals(req.body.user);
+    console.log(req.body.user);
+    res.status(200).json({
+      success: true,
+      data: expenses,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+    });
+  }
+};
 
 //@desc get single expense
 //@route GET api/v1/expense-tracker/id
 //@access PRIVATE
 exports.getExpense = async (req, res, next) => {
-    try {
-        const expense = await Expense.findById(req.params.id);
-        res.status(200).json({
-            success:true,
-            data: expense
-        })
-    } catch (error) {
-        res.status(400).json({
-            success:false
-        })
-    }
-}
+  try {
+    const expense = await Expense.findById(req.params.id);
+    res.status(200).json({
+      success: true,
+      data: expense,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+    });
+  }
+};
 
 //@desc create new expense
 //@route POST api/v1/expense-tracker
 //@access PRIVATE
 exports.createExpense = async (req, res, next) => {
-    try {
-        const expense = await Expense.create(req.body);
-        res.status(201).json({
-            success:true,
-            data: expense
-        })
-    } catch (error) {
-        res.status(400).json({success:false})
-    }
-}
+  try {
+    const expense = await Expense.create(req.body);
+    res.status(201).json({
+      success: true,
+      data: expense,
+    });
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
+};
 
 //@desc update existing expense
 //@route PUT api/v1/expense-tracker/id
 //@access PRIVATE
 exports.updateExpense = async (req, res, next) => {
-    try {
-        const expense = await Expense.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators: true
-          });
-        res.status(200).json({
-            success:true,
-            data: expense
-        })
-    } catch (error) {
-        res.status(400).json({
-            success:false
-        })
-    }
-}
+  try {
+    const expense = await Expense.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      success: true,
+      data: expense,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+    });
+  }
+};
 
 //@desc delete existing expense
 //@route DELETE api/v1/expense-tracker
 //@access PRIVATE
 exports.deleteExpense = async (req, res, next) => {
-    try {
-        const expense = await Expense.findByIdAndDelete(req.params.id);
-        res.status(200).json({
-            success:true,
-            data: expense
-        })
-    } catch (error) {
-        res.status(400).json({
-            success:false
-        })
-    }
-}
+  try {
+    const expense = await Expense.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      success: true,
+      data: expense,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+    });
+  }
+};
