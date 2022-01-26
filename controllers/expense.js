c240330fe0b30549bca77c538b90177c5987b4ab
voomoo <rakibul15-1318@diu.edu.bd>
2022-01-26 11:01:08 +0600

    reduce current balance if expense is deleted

--- controllers/expense.js
+++ controllers/expense.js
@@ -31,66 +31,70 @@ exports.getExpensesRange = async (req, res, next) => {
     const expenses = await Expense.find({
       createdAt: { $gte: d, $lte: new Date() },
     });
-    let food = { income: 0, expense: 0 };
-    let transport = { income: 0, expense: 0 };
-    let rent = { income: 0, expense: 0 };
-    let equipment = { income: 0, expense: 0 };
-    let entertainment = { income: 0, expense: 0 };
-    let education = { income: 0, expense: 0 };
-    let salary = { income: 0, expense: 0 };
-    let freelance = { income: 0, expense: 0 };
-    let gift = { income: 0, expense: 0 };
-    let parents = { income: 0, expense: 0 };
-    let others = { income: 0, expense: 0 };
+    let income = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    let expense = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    let label = [
+      "Food",
+      "Transport",
+      "Rent",
+      "Equipment",
+      "Entertainment",
+      "Education",
+      "Salary",
+      "Freelance",
+      "Gift",
+      "Parents",
+      "Others",
+    ];
 
     expenses.forEach((elem) => {
       if (elem.accountType === "expense") {
         if (elem.category === "food") {
-          food.expense += elem.amount;
+          expense[0] += elem.amount;
         } else if (elem.category === "transport") {
-          transport.expense += elem.amount;
+          expense[1] += elem.amount
         } else if (elem.category === "rent") {
-          rent.expense += elem.amount;
+          expense[2] += elem.amount
         } else if (elem.category === "equipment") {
-          equipment.expense += elem.amount;
+          expense[3] += elem.amount
         } else if (elem.category === "entertainment") {
-          entertainment.expense += elem.amount;
+          expense[4] += elem.amount
         } else if (elem.category === "education") {
-          education.expense += elem.amount;
+          expense[5] += elem.amount
         } else if (elem.category === "salary") {
-          salary.expense += elem.amount;
+          expense[6] += elem.amount
         } else if (elem.category === "freelance") {
-          freelance.expense += elem.amount;
+          expense[7] += elem.amount
         } else if (elem.category === "gift") {
-          gift.expense += elem.amount;
+          expense[8] += elem.amount
         } else if (elem.category === "parents") {
-          parents.expense += elem.amount;
+          expense[9] += elem.amount
         } else if (elem.category === "others") {
-          others.expense += elem.amount;
+          expense[10] += elem.amount
         }
       } else {
         if (elem.category === "food") {
-          food.income += elem.amount;
+          income[0] += elem.amount;
         } else if (elem.category === "transport") {
-          transport.income += elem.amount;
+          income[1] += elem.amount
         } else if (elem.category === "rent") {
-          rent.income += elem.amount;
+          income[2] += elem.amount
         } else if (elem.category === "equipment") {
-          equipment.income += elem.amount;
+          income[3] += elem.amount
         } else if (elem.category === "entertainment") {
-          entertainment.income += elem.amount;
+          income[4] += elem.amount
         } else if (elem.category === "education") {
-          education.income += elem.amount;
+          income[5] += elem.amount
         } else if (elem.category === "salary") {
-          salary.income += elem.amount;
+          income[6] += elem.amount
         } else if (elem.category === "freelance") {
-          freelance.income += elem.amount;
+          income[7] += elem.amount
         } else if (elem.category === "gift") {
-          gift.income += elem.amount;
+          income[8] += elem.amount
         } else if (elem.category === "parents") {
-          parents.income += elem.amount;
+          income[9] += elem.amount
         } else if (elem.category === "others") {
-          others.income += elem.amount;
+          income[10] += elem.amount
         }
       }
     });
@@ -98,17 +102,9 @@ exports.getExpensesRange = async (req, res, next) => {
     res.status(200).json({
       success: true,
       data: expenses,
-      food,
-      transport,
-      rent,
-      equipment,
-      entertainment,
-      education,
-      salary,
-      freelance,
-      gift,
-      parents,
-      others,
+      income,
+      expense,
+      label
     });
   } catch (error) {
     res.status(400).json({
@@ -196,6 +192,25 @@ exports.updateExpense = async (req, res, next) => {
 exports.deleteExpense = async (req, res, next) => {
   try {
     const expense = await Expense.findByIdAndDelete(req.params.id);
+    const user = await User.findById(req.user.user_id);
+    console.log(req.body.accountType);
+    if (expense.accountType === "expense") {
+      const userUpdate = await User.updateOne(
+        { _id: req.user.user_id },
+        {
+          $set: {
+            currentBalance: user.currentBalance + req.body.amount,
+            totalExpense: user.totalExpense - req.body.amount,
+          },
+        }
+      );
+      console.log(userUpdate);
+    } else {
+      const userUpdate = await User.findByIdAndUpdate(req.user.user_id, {
+        currentBalance: user.currentBalance - req.body.amount,
+        totalIncome: user.totalIncome - req.body.amount,
+      });
+    }
     res.status(200).json({
       success: true,
       data: expense,
